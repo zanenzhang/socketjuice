784a61d5038fa7b7b81f52f3393dff41df9cf353
Zanenzhang <zanzhang@seas.upenn.edu>
2023-10-29 15:10:27 -0400

    Adding main header

--- backend/model/User.js
+++ backend/model/User.js
@@ -31,6 +31,10 @@ const userSchema = new Schema({
         type: Boolean, //Not verified
         default: false
     },
+    waitingIdApproval: {
+        type: Boolean,
+        default: false
+    },
     deactivated: {
         type: Boolean, //Banned
         default: false
